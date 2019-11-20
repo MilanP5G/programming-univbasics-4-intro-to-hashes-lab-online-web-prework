9fefe2e2ad388daff1e1bf5b3e8adfd53ef6098c
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-11-20 18:34:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,10 +36,10 @@ end
 
 def update_counting_hash(hash, key)
   
-  if update_counting_hash[key]
-  update_counting_hash[key] += 1
+  if update_counting_hash[hello]
+  update_counting_hash[hello] += 1
 else
-  update_counting_hash[key] = 1
+  update_counting_hash[hello] = 1
 end
 
 end
